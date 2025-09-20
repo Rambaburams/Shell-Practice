2ca43b1bd4a2e20c58558a12b86106b36069fb48
unknown <prasadnvv3@gmail.com>
2025-09-20 15:19:07 +0530

    variable

---
+++ 4.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1:: Hi $Person2 how are you?"
+echo "$Person2:: I'm fine $Person1 what about you?"
+echo "$Person1:: That's good! $Person2 anything else"
+echo "$Person2:: Nothing much $Person1"
