94546bcf6ffe58fbda17196d40e4154a830ebbd7
unknown <prasadnvv3@gmail.com>
2025-09-20 13:14:55 +0530

    added convresations

---
+++ 2.conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi suresh how are you?"
+echo "Suresh:: I'm fine Ramesh what about you?"
+echo "Ramesh:: That's good! Suresh anything else"
+echo "Suresh:: Nothing much Ramesh"
