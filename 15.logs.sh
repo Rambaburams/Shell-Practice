09244f74fd25639ae3721711fd613377ff7614d7
unknown <prasadnvv3@gmail.com>
2025-09-22 23:40:26 +0530

    condition

---
+++ 15.logs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+User=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+Logs_floder="/var/log/shell-script"
+script_name=$ (echo $0 | cut -d "." -f1)
+Log_filename="$Logs_floder/$script_name.log"
+
+mkdir -p $Logs_floder
+echo "script started at :$(date)"
+
+if [ $User -ne 0 ] ; then
+    echo "Error : please run the script with root privillages"
+    exit 1
+fi
