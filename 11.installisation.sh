4b804ec2897d0b0be5e47a5801a4f0b03b98ee1f
unknown <prasadnvv3@gmail.com>
2025-09-22 22:40:32 +0530

    condition

--- 11.installisation.sh
+++ 11.installisation.sh
@@ -5,3 +5,10 @@ User=$(id -u)
 if [ $User -ne 0 ] ; then
     echo "ERROR:: please Run the command from root privilages"
 fi
+
+dnf install python3 -y
+
+if [ $? -ne 0 ] ; then
+    echo "ERROR: Installing python was failed"
+else 
+    echo "installation was success"
