a22197497e8b3b59cbe483ee058353f921efda22
unknown <prasadnvv3@gmail.com>
2025-09-22 22:32:23 +0530

    condition

--- 10.conditions.sh
+++ 10.conditions.sh
@@ -2,8 +2,8 @@
 
 echo "Please Enter the Number"
 read Number
-if [$((Number%2)) -eq 0] ; then 
-    echo"the Given $Number is even "
+if [ $((Number%2)) -eq 0 ] ; then 
+    echo "the Given $Number is even "
 else 
     echo  "the Given $Number is odd"
 fi
