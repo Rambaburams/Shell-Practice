1a72dfb8546b84bc21b419da8eeecc96d9e19734
unknown <prasadnvv3@gmail.com>
2025-09-29 18:59:49 +0530

    condition

--- 20.disk_usage.sh
+++ 20.disk_usage.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 Disk_Usage=$(df -hT | grep -v Filesystem)
-echo "$Disk_Usage"
+# echo "$Disk_Usage"
+DISK_THRESHOLD=2 # in project we keep it as 75
+IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+MESSAGE=""
 
 while IFS= read -r line
 do
-    USAGE=$(echo $line  | awk '{print $6}')
+    USAGE=$(echo $line  | awk '{print $6}' | cut -d '%' -f1)
+    partitaion=$(echo $line |awk '{print $7}')
+    if [ $USAGE -ge $DISK_THRESHOLD ] ; then
+        MESSAGE+="High disk usage on $partitaion: $USAGE % \n"
+    fi
 done <<< Disk_Usage
+
+echo -e "Message Body: $MESSAGE"
